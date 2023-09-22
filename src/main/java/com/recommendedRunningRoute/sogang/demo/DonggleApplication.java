d30348320c4af05dfde2b69817aa02da030e8222
DongHyun <kimdh0315@naver.com>
2023-09-22 15:25:47 +0900

    initial upload

---
+++ src/main/java/com/recommendedRunningRoute/sogang/demo/DonggleApplication.java
@@ -0,0 +1,13 @@
+package com.recommendedRunningRoute.sogang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonggleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recommendedRunningRoute/sogang/demo/DonggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommendedRunningRoute.sogang.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
